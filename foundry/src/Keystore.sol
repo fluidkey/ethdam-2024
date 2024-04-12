e19d55bce4bae527aa1ae2cd2aaadfb519b8c8af
moritz <moritz@fluidkey.com>
2024-04-12 14:01:22 +0200

    initialize foundry

--- foundry/src/Keystore.sol
+++ foundry/src/Keystore.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
 
-contract Counter {
+contract Keystore {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
