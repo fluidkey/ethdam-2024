47014cdc141d01ee767227b9193fd9bffe77336c
Federico Castelli <federico@fluidkey.com>
2024-04-13 21:18:10 +0200

    feat: update cpu and memory for fargate task

--- docker-prover/src/backend-docker-prover.ts
+++ docker-prover/src/backend-docker-prover.ts
@@ -32,8 +32,8 @@ export class DockerProverStack extends Stack {
       'ECSTaskDefinition',
       {
         family: 'docker-prover-task',
-        cpu: '256',
-        memoryMiB: '1024',
+        cpu: '1024',
+        memoryMiB: '4096',
         runtimePlatform: {
           cpuArchitecture: aws_ecs.CpuArchitecture.X86_64,
           operatingSystemFamily: aws_ecs.OperatingSystemFamily.LINUX,
