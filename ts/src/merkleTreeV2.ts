be8b6b436683f40a92c0cdcb9572c63548a9f9a7
moritz <moritz@fluidkey.com>
2024-04-13 15:45:14 +0200

    work on keystore

--- ts/src/merkleTreeV2.ts
+++ ts/src/merkleTreeV2.ts
@@ -5,6 +5,7 @@ export const generateZeroLeaves = (): `0x${string}`[] => {
   let leaves: `0x${string}`[] = []
   for (let i = 0; i < 8; i++) {
     const leaf = generateLeaf(i, new Uint8Array(32), new Uint8Array(32));
+    console.log('leaf: ', leaf);
     leaves.push(leaf);
   }
   return leaves;
@@ -12,7 +13,9 @@ export const generateZeroLeaves = (): `0x${string}`[] => {
 
 export const generateLeaf = (index: number, pubKeyX: Uint8Array, pubKeyY: Uint8Array): `0x${string}` => {
   const leafIndex = toHex(index, { size: 32 });
+  console.log('leafIndex: ', leafIndex);
   const leafPublicKeyHash = keccak256(concat([pubKeyX, pubKeyY]));
+  console.log('leafPublicKeyHash: ', leafPublicKeyHash);
   return keccak256(concat([leafPublicKeyHash, leafIndex])) as `0x${string}`;
 }
 
